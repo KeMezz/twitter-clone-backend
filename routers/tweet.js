2eeb5b01dedfa51215ab85d1e86022a9e845ac33
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-18 02:17:07 +0900

    트윗 수정 기능 추가

--- routers/tweet.js
+++ routers/tweet.js
@@ -20,16 +20,39 @@ tweetRouter.post("/", (req, res) => {
   }
 });
 
-tweetRouter.delete("/", (req, res) => {
-  const id = req.body?.id.toString();
+tweetRouter.put("/", (req, res) => {
+  const id = req.body?.id?.toString();
+  const text = req.body?.text;
   const targetIndex = tweets.findIndex((tweet) => tweet.id.toString() === id);
-  if (!id) {
-    return res.sendStatus(400);
+
+  if (!id || !text) {
+    return res
+      .status(400)
+      .send(
+        "can't find tweet id or new tweet text in your request (need both)"
+      );
   } else if (targetIndex < 0) {
-    return res.status(404).send("Tweet not found");
+    return res.status(404).send("Requested tweet id is not exists");
+  } else {
+    const newTweet = {
+      ...tweets.find((tweet) => tweet.id.toString() === id),
+      text,
+    };
+    tweets.splice(targetIndex, 1, newTweet);
+    return res.status(201).send(tweets);
+  }
+});
+
+tweetRouter.delete("/", (req, res) => {
+  const id = req.body?.id?.toString();
+  const targetIndex = tweets.findIndex((tweet) => tweet.id.toString() === id);
+
+  if (!id) {
+    return res.status(400).send("can't find tweet id in your request");
+  } else if (targetIndex < 0) {
+    return res.status(404).send("Requested tweet id is not exists");
   } else {
     tweets.splice(targetIndex, 1);
-    // const newTweets = tweets.filter((tweet) => tweet.id.toString() !== id);
     return res.status(200).send(tweets);
   }
 });
