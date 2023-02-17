6d27942b0aba1b8e486ab84f5e6343f842f5d581
KeMezz <pnescene@icloud.com>
2023-02-17 13:52:29 +0900

    tweets 배열 별도 분리

--- routers/tweets.js
+++ routers/tweets.js
@@ -1,27 +1,7 @@
 import express from "express";
+import { tweets } from "../stores/tweets";
 
 const tweetsRouter = express.Router();
-
-const tweets = [
-  {
-    id: 1676444678643,
-    username: "Bob",
-    text: "안녕 반가워 :)",
-    createdAt: "2023-02-15T07:04:38.643Z",
-  },
-  {
-    id: 1676444762544,
-    username: "hyeongjin",
-    text: "진짜 신기하다!!",
-    createdAt: "2023-02-15T07:06:02.544Z",
-  },
-  {
-    id: 1676444762544,
-    username: "hyeongjin",
-    text: "진짜 신기하다!!",
-    createdAt: "2023-02-15T07:06:02.544Z",
-  },
-];
 
 tweetsRouter.get("/", (req, res) => {
   const username = req.query?.username;
---
+++ stores/tweets.js
@@ -0,0 +1,20 @@
+export const tweets = [
+  {
+    id: 1676444678643,
+    username: "Bob",
+    text: "안녕 반가워 :)",
+    createdAt: "2023-02-15T07:04:38.643Z",
+  },
+  {
+    id: 1676444762544,
+    username: "hyeongjin",
+    text: "진짜 신기하다!!",
+    createdAt: "2023-02-15T07:06:02.544Z",
+  },
+  {
+    id: 1676444762544,
+    username: "hyeongjin",
+    text: "진짜 신기하다!!",
+    createdAt: "2023-02-15T07:06:02.544Z",
+  },
+];
