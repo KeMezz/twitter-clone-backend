966f4504318cfda33e70c368762036bc57769e5e
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-19 13:29:49 +0900

    install morgan, helmet, express async errors

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import "express-async-errors";
 import tweetRouter from "./routers/tweet.js";
 import tweetsRouter from "./routers/tweets.js";
 
@@ -7,6 +10,8 @@ const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(helmet());
+app.use(morgan("tiny"));
 app.use(
   cors({
     origin: ["http://localhost:3000"],
@@ -18,5 +23,14 @@ app.use(
 // routers
 app.use("/tweets", tweetsRouter);
 app.use("/tweet", tweetRouter);
+
+// error handlers
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.sendStatus(500);
+});
 
 app.listen(8080);
