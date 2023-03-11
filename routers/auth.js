9994a6b38f3884e60ceed10f6782763286c04cb8
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-11 23:32:45 +0900

    controller 분리, 유효성 검사 추가

--- routers/auth.js
+++ routers/auth.js
@@ -1,46 +1,34 @@
 import express from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { users } from "../data/users.js";
+import * as authController from "../controller/auth.js";
+import {
+  findErrors,
+  validateBodyUsername,
+  validatePassword,
+  validateUrl,
+} from "../middleware/validator.js";
 
 const authRouter = express.Router();
-const secret = "Vw-fxufR8zoFK-iYVsyzHbXMo2pk_pr!";
 
-authRouter.post("/signup", (req, res, next) => {
-  const { username, password, url } = req.body;
-  bcrypt.hash(password, 10).then((bcrypted) => {
-    const newUser = {
-      username,
-      password: bcrypted,
-      url,
-    };
-    users.push(newUser);
-    return res.status(201).send(users);
-  });
-});
+authRouter.post(
+  "/signup",
+  [validateBodyUsername, validatePassword, validateUrl, findErrors],
+  authController.signup
+);
+authRouter.post(
+  "/login",
+  [validateBodyUsername, validatePassword, findErrors],
+  authController.login
+);
 
-authRouter.post("/login", (req, res, next) => {
-  const { username, password } = req.body;
-  const user = users.find((user) => user.username === username);
-  bcrypt.compare(password, user.password).then((isMatched) => {
-    if (isMatched) {
-      const token = jwt.sign({ username }, secret, { expiresIn: 1000 });
-      return res.status(200).send({ token });
-    } else {
-      return res.sendStatus(404);
-    }
-  });
-});
-
-authRouter.post("/me", (req, res, next) => {
-  const { token } = req.body;
-  jwt.verify(token, secret, (error) => {
-    if (error) {
-      return res.sendStatus(404);
-    } else {
-      return res.sendStatus(200);
-    }
-  });
-});
+// authRouter.post("/me", (req, res, next) => {
+//   const { token } = req.body;
+//   jwt.verify(token, secret, (error) => {
+//     if (error) {
+//       return res.sendStatus(404);
+//     } else {
+//       return res.sendStatus(200);
+//     }
+//   });
+// });
 
 export default authRouter;
