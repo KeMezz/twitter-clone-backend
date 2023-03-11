b4cc383681fde770aef383da666da4b56a7bb8e1
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-11 16:27:26 +0900

    Create users.js

---
+++ data/users.js
@@ -0,0 +1,17 @@
+export let users = [
+  {
+    username: "hyeongjin",
+    password: "$2b$10$1qNZOLvPWHc7BYtO20UWCuzIzqmfqRchnh0XdJsNzeoA.EjUJKYna",
+    url: "",
+  },
+  {
+    username: "bob",
+    password: "$2b$10$DViy9oZJvmecy.I1MCCiZeWSkMIVLfWEsH770BNevxS7OtGJmADpO",
+    url: "",
+  },
+  {
+    username: "ellie",
+    password: "$2b$10$75HaC6avTpw/cJDSwjalQuxMir/R1L9v9QiRBtv8FO1vDwJXntFBe",
+    url: "",
+  },
+];
