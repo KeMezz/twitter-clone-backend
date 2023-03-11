a17980f64cea0dcc20c52018a70a2860ceea6075
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-11 23:32:27 +0900

    user db 업데이트 함수 추가

--- data/users.js
+++ data/users.js
@@ -1,17 +1,28 @@
 export let users = [
   {
+    id: "",
     username: "hyeongjin",
     password: "$2b$10$1qNZOLvPWHc7BYtO20UWCuzIzqmfqRchnh0XdJsNzeoA.EjUJKYna",
     url: "",
   },
   {
+    id: "",
     username: "bob",
     password: "$2b$10$DViy9oZJvmecy.I1MCCiZeWSkMIVLfWEsH770BNevxS7OtGJmADpO",
     url: "",
   },
   {
+    id: "",
     username: "ellie",
     password: "$2b$10$75HaC6avTpw/cJDSwjalQuxMir/R1L9v9QiRBtv8FO1vDwJXntFBe",
     url: "",
   },
 ];
+
+export const create = async (newUser) => {
+  users.push(newUser);
+};
+
+export const findByUsername = async (username) => {
+  return users.find((user) => user.username === username);
+};
