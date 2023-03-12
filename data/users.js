bb99bff2714035e3495a93aaca700a44ca92aa0a
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-13 07:37:23 +0900

    id 기준 선별 함수 추가

--- data/users.js
+++ data/users.js
@@ -1,18 +1,18 @@
 export let users = [
   {
-    id: "",
+    id: "1",
     username: "hyeongjin",
     password: "$2b$10$1qNZOLvPWHc7BYtO20UWCuzIzqmfqRchnh0XdJsNzeoA.EjUJKYna",
     url: "",
   },
   {
-    id: "",
+    id: "2",
     username: "bob",
     password: "$2b$10$DViy9oZJvmecy.I1MCCiZeWSkMIVLfWEsH770BNevxS7OtGJmADpO",
     url: "",
   },
   {
-    id: "",
+    id: "3",
     username: "ellie",
     password: "$2b$10$75HaC6avTpw/cJDSwjalQuxMir/R1L9v9QiRBtv8FO1vDwJXntFBe",
     url: "",
@@ -21,6 +21,14 @@ export let users = [
 
 export const create = async (newUser) => {
   users.push(newUser);
+};
+
+export const getAllById = async (id) => {
+  return users.filter((user) => user.id === id);
+};
+
+export const getById = async (id) => {
+  return users.find((user) => user.id === id);
 };
 
 export const findByUsername = async (username) => {
