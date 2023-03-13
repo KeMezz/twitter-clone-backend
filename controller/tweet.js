4ae79fe5a4934f2fcd12587b983567462d5bc82e
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-13 21:49:59 +0900

    userId로 트윗 수정, 삭제시 본인 검사

--- controller/tweet.js
+++ controller/tweet.js
@@ -20,7 +20,8 @@ export const getTweet = async (req, res) => {
 };
 
 export const createTweet = async (req, res) => {
-  const { userId, text } = req.body;
+  const { userId } = req;
+  const { text } = req.body;
   const tweet = await tweetRepository.create(userId, text);
   if (tweet) {
     res.status(201).send(tweet);
@@ -30,20 +31,26 @@ export const createTweet = async (req, res) => {
 };
 
 export const updateTweet = async (req, res) => {
+  const { userId } = req;
   const { id, text } = req.body;
   const tweet = await tweetRepository.update(id, text);
   if (tweet) {
     res.status(201).send(await tweetRepository.update(id, text));
+  } else if (userId !== tweet.userId) {
+    res.status(401).json(`can't update other person's tweet`);
   } else {
     res.status(404).send(`tweet id ${id} not found`);
   }
 };
 
 export const removeTweet = async (req, res) => {
+  const { userId } = req;
   const { id } = req.body;
-  const tweets = await tweetRepository.remove(id);
-  if (tweets) {
-    res.status(204).send(tweets);
+  const tweet = await tweetRepository.remove(id);
+  if (tweet) {
+    res.sendStatus(204);
+  } else if (userId !== tweet.userId) {
+    res.status(401).json(`can't delete other person's tweet`);
   } else {
     res.status(404).send(`tweet id ${id} is not found.`);
   }
