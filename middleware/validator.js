84789a7a0344abbc5bbca31d1a065b7b87d22249
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-14 22:32:56 +0900

    유효성 검사 메시지 업데이트

--- middleware/validator.js
+++ middleware/validator.js
@@ -11,27 +11,27 @@ export const findErrors = (req, res, next) => {
 
 export const validateUsername = body("username")
   .trim()
-  .isLength({ min: 4 })
-  .withMessage("username을 입력해주세요.");
+  .isLength({ min: 4, max: 12 })
+  .withMessage("username은 4자 이상, 12자 이하여야 합니다.");
 
 export const validateText = body("text")
   .trim()
-  .isLength({ max: 200 })
-  .withMessage("트윗 내용은 200자를 넘을 수 없습니다.");
+  .isLength({ max: 120 })
+  .withMessage("트윗 내용은 120자를 넘을 수 없습니다.");
 
 export const validateId = body("id")
   .isNumeric()
-  .withMessage("id 형식이 올바르지 않습니다.");
+  .withMessage("ID 형식이 올바르지 않습니다.");
 
 export const validateUserId = body("userId")
   .exists()
   .withMessage("userId를 입력해주세요.");
 
 export const validatePassword = body("password")
-  .isLength({ min: 5 })
-  .withMessage("Password too short.");
+  .isLength({ min: 5, max: 22 })
+  .withMessage("비밀번호는 최소 5자 이상, 최대 22자 이하여야 합니다.");
 
 export const validateUrl = body("url")
   .isURL()
-  .withMessage("invalid URL")
+  .withMessage("URL 형식이 올바르지 않습니다.")
   .optional({ nullable: true, checkFalsy: true });
