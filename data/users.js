f01aa8868152dee6952a87f7ecf6842080228256
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-19 15:42:49 +0900

    User 테이블 생성 코드 이동

--- data/users.js
+++ data/users.js
@@ -1,4 +1,26 @@
-import { User } from "../db/database.js";
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db/database.js";
+
+export const User = sequelize.define("users", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING(45),
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(128),
+    allowNull: false,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+});
 
 export const create = async (user) => {
   return User.create(user).then(({ dataValues }) => dataValues.id);
