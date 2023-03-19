0b0e91efd07e61807433fc906effb13a26c00528
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-19 15:42:39 +0900

    테이블 생성 코드 분리

--- db/database.js
+++ db/database.js
@@ -1,6 +1,5 @@
-import mysql from "mysql2";
 import { config } from "../config.js";
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize } from "sequelize";
 
 const { database, user, password, host, port } = config.db;
 
@@ -9,51 +8,3 @@ export const sequelize = new Sequelize(database, user, password, {
   port,
   dialect: "mysql",
 });
-
-export const User = sequelize.define("users", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING(45),
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING(128),
-    allowNull: false,
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-});
-
-export const Tweet = sequelize.define("tweets", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  text: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-  },
-});
-
-const pool = mysql.createPool({
-  host: config.db.host,
-  port: config.db.port,
-  user: config.db.user,
-  database: config.db.database,
-  password: config.db.password,
-});
-
-export const db = pool.promise();
