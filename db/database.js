b2eb3aa1836f4aa4bebb39a19922d3d102026f67
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-19 14:43:06 +0900

    set and connect sequelize

--- db/database.js
+++ db/database.js
@@ -1,5 +1,14 @@
 import mysql from "mysql2";
 import { config } from "../config.js";
+import { Sequelize } from "sequelize";
+
+const { database, user, password, host, port } = config.db;
+
+export const sequelize = new Sequelize(database, user, password, {
+  host,
+  port,
+  dialect: "mysql",
+});
 
 const pool = mysql.createPool({
   host: config.db.host,
