31502ea3f2e5c75b304386cc702ac6bdf0190f5d
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-27 21:39:30 +0900

    users, tweets 컬렉션 생성

--- db/database.js
+++ db/database.js
@@ -1,8 +1,18 @@
-import MongoDb from "mongodb";
+import { MongoClient } from "mongodb";
 import { config } from "../config.js";
 
+let db;
+const client = new MongoClient(config.db.host);
+
 export async function connectDB() {
-  return MongoDb.MongoClient.connect(config.db.host).then((client) =>
-    client.db()
-  );
+  db = client.db("twiter_clone");
+  return db;
+}
+
+export function getUsers() {
+  return db.collection("users");
+}
+
+export function getTweets() {
+  return db.collection("tweets");
 }
